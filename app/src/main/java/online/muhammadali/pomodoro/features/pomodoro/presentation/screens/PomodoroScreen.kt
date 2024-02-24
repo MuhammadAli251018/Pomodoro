c84235bfd7b64d72d27887b33dc012210cc8869e
MuhammadAli251018 <muhammadali251018@gmail.com>
2024-02-24 09:32:35 +0200

    Edit the mainifest to new Acitivity location
    Created the features package and add pomodoro screen file

---
+++ app/src/main/java/online/muhammadali/pomodoro/features/pomodoro/presentation/screens/PomodoroScreen.kt
@@ -0,0 +1,15 @@
+package online.muhammadali.pomodoro.features.pomodoro.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PomodoroScreen() {
+
+}
+
+@Preview
+@Composable
+fun PomodoroScreenPreview() {
+
+}
