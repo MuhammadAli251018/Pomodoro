da01039ac92472edc456e2677c819f2fc47d68d2
MuhammadAli251018 <muhammadali251018@gmail.com>
2024-03-01 14:35:51 +0200

    Add PomodoroScreen with state/action manager
    This commit completes the PomodoroScreen

--- app/src/main/java/online/muhammadali/pomodoro/features/pomodoro/presentation/screens/PomodoroScreen.kt
+++ app/src/main/java/online/muhammadali/pomodoro/features/pomodoro/presentation/screens/PomodoroScreen.kt
@@ -3,10 +3,10 @@ package online.muhammadali.pomodoro.features.pomodoro.presentation.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,13 +14,119 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.StateFlow
 import online.muhammadali.pomodoro.common.presentation.components.VerticalSpace
 import online.muhammadali.pomodoro.common.ui.theme.PomodoroTheme
 import online.muhammadali.pomodoro.features.pomodoro.presentation.components.PomodoroCounter
 import online.muhammadali.pomodoro.features.pomodoro.presentation.components.SessionsIndicator
 
+
+sealed class PomodoroScreenMode {
+    @get:Composable
+    abstract val backgroundColor: Color
+    abstract val title: String
+
+    data object FocusMode : PomodoroScreenMode() {
+        override val backgroundColor: Color
+            @Composable
+            get() {
+               return MaterialTheme.colorScheme.secondary
+            }
+
+        override val title: String = "Focus Time"
+    }
+
+    data object LongBreakMode : PomodoroScreenMode() {
+        override val backgroundColor: Color
+            @Composable
+            get() {
+                return MaterialTheme.colorScheme.tertiary
+            }
+
+        override val title: String = "Long Break Time"
+    }
+
+    data object ShortBreakMode : PomodoroScreenMode() {
+        override val backgroundColor: Color
+            @Composable
+            get() {
+                return MaterialTheme.colorScheme.background
+            }
+
+        override val title: String = "Break Time"
+    }
+}
+
+data class IndicatorState(
+    val sessionsCount: Int,
+    val lastCompletedSession: Int
+)
+
+data class PomodoroCounterState (
+    val currentTime: String,
+    val completion: Float
+) {
+    sealed class State {
+
+        abstract fun reverse(): State
+        data object OnGoing : State() {
+            override fun reverse(): State {
+                return Paused
+            }
+        }
+
+        data object Paused : State() {
+            override fun reverse(): State {
+                return OnGoing
+            }
+        }
+    }
+}
+
+interface PomodoroScreenSAManager {
+    val screenMode: StateFlow<PomodoroScreenMode>
+    val counterState: StateFlow<PomodoroCounterState>
+    val indicatorState: StateFlow<IndicatorState>
+    val onCounterClick: () -> Unit
+}
+
 @Composable
 fun PomodoroScreen(
+    stateActionManager: PomodoroScreenSAManager
+) {
+    val screenMode by stateActionManager.screenMode.collectAsStateWithLifecycle()
+    val counterState by stateActionManager.counterState.collectAsStateWithLifecycle()
+    val indicatorState by stateActionManager.indicatorState.collectAsStateWithLifecycle()
+
+    PomodoroScreen(
+        screenMode = screenMode,
+        counterState = counterState,
+        indicatorState = indicatorState,
+        onCounterClick = stateActionManager.onCounterClick
+    )
+}
+@Composable
+fun PomodoroScreen(
+    screenMode: PomodoroScreenMode,
+    counterState: PomodoroCounterState,
+    indicatorState: IndicatorState,
+    onCounterClick: () -> Unit
+) {
+    PomodoroScreen(
+        backgroundColor = screenMode.backgroundColor,
+        time = counterState.currentTime,
+        counterCompletion = counterState.completion,
+        sessionsCount = indicatorState.sessionsCount,
+        lastCompletedSession = indicatorState.lastCompletedSession,
+        title = screenMode.title,
+        onCounterClick = onCounterClick
+    )
+}
+
+// stateless
+@Composable
+private fun PomodoroScreen(
     backgroundColor: Color,
     time: String,
     counterCompletion: Float,
